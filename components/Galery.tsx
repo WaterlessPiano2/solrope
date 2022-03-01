b5186d2a50a7b847c2517886b4b0fae6093551af
Chad Laptop <scderya@gmail.com>
2022-03-01 22:40:23 +0100

    fetch nft data and show them WIP

--- components/Galery.tsx
+++ components/Galery.tsx
@@ -2,14 +2,36 @@ import * as React from "react";
 
 const Galery = ({ nfts }) => {
   console.log(nfts);
+  const [metadata, setMetadata] = React.useState({});
+
+  const fetchMetadata = React.useCallback(async () => {
+    for (const nft of nfts) {
+      fetch(nft.data.uri)
+        .then((response) => response.json())
+        .then((meta) =>
+          setMetadata((state) => ({ ...state, [nft.mint]: meta }))
+        );
+    }
+  }, [nfts]);
+
+  React.useEffect(() => {
+    if (nfts?.length) fetchMetadata();
+  }, [nfts, fetchMetadata]);
+
   return (
     <div>
-      {nfts &&
-        nfts.map((x) => (
-          <img
-            src={x.data.uri}
-          />
-        ))}
+      <h1>Solana NFT List</h1>
+      <hr />
+      {(nfts || []).map((nft) => {
+        console.log(metadata?.[nft.mint]?.image);
+        return (
+          <div key={nft.mint}>
+            <img src={metadata?.[nft.mint]?.image} alt="nft" width="200" />
+            <br />
+            {nft.data.name}
+          </div>
+        );
+      })}
     </div>
   );
 };
